e57c0a7288a567ec2e2467b47b899eba5029a4b6
Tony <thanhfourteen@gmail.com>
2025-03-17 06:57:26 +0700

    debug val

--- segment/val_detr.py
+++ segment/val_detr.py
@@ -261,7 +261,9 @@ def run(
     callbacks.run('on_val_start')
     pbar = tqdm(dataloader, desc=s, bar_format=TQDM_BAR_FORMAT)
     for batch_i, (im, targets, paths, shapes, masks) in enumerate(pbar):
-        print(f"Batch {batch_i} - masks: {len(masks)}, mask shape: {masks[0].shape if masks else None}")
+        print(f"Batch {batch_i} - masks type: {type(masks)}, masks: {masks}")
+        print(f"Batch {batch_i} - masks length: {len(masks) if not torch.is_tensor(masks) else masks.numel()}, "
+            f"mask shape: {masks[0].shape if len(masks) > 0 else None}")
         callbacks.run('on_val_batch_start')
         with dt[0]:
             if cuda:
