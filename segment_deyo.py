407a44fd5f2bb07f1fe60e04b1e9d1ebf8dfa8e7
Tony <thanhfourteen@gmail.com>
2025-03-17 09:12:19 +0700

    check shape mask

--- segment_deyo.py
+++ segment_deyo.py
@@ -58,7 +58,6 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     
     overlap = not opt.no_overlap
     print(f"overlap {overlap}")
-    exit()
 
     # callbacks.run('on_pretrain_routine_start')
 
@@ -279,6 +278,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             pbar = tqdm(pbar, total=nb, bar_format=TQDM_BAR_FORMAT)  # progress bar
         optimizer.zero_grad()
         for i, (imgs, targets, paths, _, masks) in pbar:  # batch ------------------------------------------------------
+            print(f"\nmask shape {masks.shape}\n")
+            exit()
             # callbacks.run('on_train_batch_start')
             ni = i + nb * epoch  # number integrated batches (since train start)
             imgs = imgs.to(device, non_blocking=True).float() / 255  # uint8 to float32, 0-255 to 0.0-1.0
