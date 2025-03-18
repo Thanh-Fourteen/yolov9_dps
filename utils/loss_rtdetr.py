f1b2a70a1c9a200ae148992f22f7a63458cd4bfc
Tony <thanhfourteen@gmail.com>
2025-03-18 09:58:44 +0700

    fix loss mask

--- utils/loss_rtdetr.py
+++ utils/loss_rtdetr.py
@@ -143,8 +143,8 @@ class HungarianMatcher(nn.Module):
         out_mask = F.grid_sample(masks.detach(), sample_points, align_corners=False).squeeze(-2)
         out_mask = out_mask.flatten(0, 1)
     
-        # tgt_mask = torch.cat(gt_mask).unsqueeze(1)
-        tgt_mask = gt_mask.unsqueeze(1)
+        tgt_mask = torch.cat(gt_mask).unsqueeze(1)
+        # tgt_mask = gt_mask.unsqueeze(1)
         sample_points = torch.cat([a.repeat(b, 1, 1, 1) for a, b in zip(sample_points, num_gts) if b > 0])
         sample_points = sample_points.to(tgt_mask.device)
         if tgt_mask.dtype != sample_points.dtype:
