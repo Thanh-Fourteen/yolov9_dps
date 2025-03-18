fb249117787203a0deecb8c1c8bb6a2c030b1000
Tony <thanhfourteen@gmail.com>
2025-03-18 09:16:26 +0700

    check shape

--- segment_deyo.py
+++ segment_deyo.py
@@ -317,13 +317,13 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                     #         mask_slices = torch.empty(0)
                     #     unique_masks.append(mask_slices)
                     
-                    bb = targets[:,2:].to(device)
-                    cls =  targets[:,1].to(device, dtype=torch.long)
-                    print(f"\ncls shape: {cls.shape}")
-                    print(f"\nbb shape: {bb.shape}")
-                    print(f"\nmasks shape: {masks.shape}")
-                    print(f"\ngt_groups shape: {len(gt_groups)}")
-                    exit()
+                    # bb = targets[:,2:].to(device)
+                    # cls =  targets[:,1].to(device, dtype=torch.long)
+                    # print(f"\ncls shape: {cls.shape}")
+                    # print(f"\nbb shape: {bb.shape}")
+                    # print(f"\nmasks shape: {masks.shape}")
+                    # print(f"\ngt_groups shape: {len(gt_groups)}")
+                    # exit()
                    
                     _targets = {
                         "cls": targets[:,1].to(device, dtype=torch.long),
