77ee0ce74231480cbf9a32c4580474c42903e804
Tony <thanhfourteen@gmail.com>
2025-03-20 11:30:50 +0700

    clear pred_masks

--- segment/val_detr.py
+++ segment/val_detr.py
@@ -97,7 +97,8 @@ def process_batch(detections, labels, iouv, pred_masks=None, gt_masks=None, over
 
     for i in range(len(iouv)):
         if masks and pred_masks is not None and gt_masks is not None:
-            x = torch.where((iou >=iouv[i]) & (mask_iou_vals >= iouv[i]) & correct_class)
+            # x = torch.where((iou >=iouv[i]) & (mask_iou_vals >= iouv[i]) & correct_class)
+            x = torch.where((iou >= iouv[i]) & correct_class) 
         else:
             x = torch.where((iou >= iouv[i]) & correct_class) # IoU > threshold and classes match
         if x[0].shape[0]:
